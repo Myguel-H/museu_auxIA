dd757fd9c886034207081dd5ffc306704a76d9e0
Myguel <Myguelhenry05@gmail.com>
2025-08-28 09:27:51 -0300

    Update mock-data.ts

--- lib/mock-data.ts
+++ lib/mock-data.ts
@@ -9,7 +9,7 @@ export const mockArtworks = [
     period: "Julho",
     dimensions: "77 cm × 53 cm",
     location: "Fazenda de morango",
-    value: Sentimental,
+    value: 0,
     description: "Retrato da coleta de morango durante o Projeto Rondon",
     image: "/coleta_morango.JPG",
     conservation: "Excelente",
